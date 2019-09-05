18f7570a7981852c0a584049757fdb304d3bd09b
ossuchas <suchat.s14102526@gmail.com>
2019-09-05 15:47:11 +0700

    mod#3

--- BatchHappyRefundSendSMS.py
+++ BatchHappyRefundSendSMS.py
@@ -4,8 +4,16 @@ import logging
 import urllib
 import pyodbc
 
+import pandas as pd
+from sqlalchemy import create_engine
+from datetime import datetime
+
+import uuid
+import random
+
 APP_NAME = "CRMREFUND"
 APIURL = 'http://192.168.0.40/smsapi/api/SMS/SendSMS'
+
 
 class ConnectDB:
     def __init__(self):
@@ -62,17 +70,15 @@ def send_sms(dataobj):
 def getDfltParam():
     """
     index value
-    0 = Subject Mail for TH
-    1 = Body Mail for TH
+    0 = SMS for TH
+    1 = SMS for Eng
     2 = Log Path for TH
-    3 = Subject Mail for EN
-    4 = Body Mail for EN
     """
 
     strSQL = """
     SELECT remarks
     FROM dbo.CRM_Param
-    WHERE param_code = 'CRM_CS_REFUND'
+    WHERE param_code = 'CRM_SMS_REFUND'
     ORDER BY param_seqn
     """
 
@@ -86,53 +92,144 @@ def getDfltParam():
     return returnVal
 
 
-def main():
+def getListData():
+
+    strSQL = """
+    SELECT hyrf_id
+    FROM dbo.crm_contact_refund
+    WHERE 1=1
+	  AND ISNULL(sms_sent_status,'N') NOT IN ('Y','E')
+	  ORDER BY createdate
+    """
+
+    myConnDB = ConnectDB()
+    result_set = myConnDB.query(strSQL)
+    returnVal = []
+
+    for row in result_set:
+        returnVal.append(row.hyrf_id)
+
+    return returnVal
+
+
+def updateRefund(hyrf_id, send_status):
+    myConnDB = ConnectDB()
+
+    params = (send_status, hyrf_id)
+
+    sqlStmt = """
+    UPDATE dbo.crm_contact_refund
+    SET sms_sent_status = ?,
+        sms_sent_date = GETDATE()
+    WHERE hyrf_id = ?
+    """
+    myConnDB.exec_sp(sqlStmt, params)
+    return
+
+
+def main(smsTH: str, smsEN: str):
+    # Get Data List
+    hyrfs = getListData()
+
+    if not hyrfs:
+        logging.info("No Data found to process Data")
+
+    params = 'Driver={ODBC Driver 17 for SQL Server};Server=192.168.0.75;Database=db_iconcrm_fusion;uid=iconuser;pwd=P@ssw0rd;'
+    params = urllib.parse.quote_plus(params)
+
+    db = create_engine('mssql+pyodbc:///?odbc_connect=%s' % params, fast_executemany=True)
+
+    for hyrf in hyrfs:
+
+        str_sql = """
+        SELECT a.foreigner, a.mobile, a.transfernumber
+        FROM dbo.crm_contact_refund a
+        WHERE a.hyrf_id = {}
+        """.format(hyrf)
+
+        df = pd.read_sql(sql=str_sql, con=db)
+
+        # assign variable
+        foreigner = df.iat[0, 0]
+        # mobile = df.iat[0, 1]
+        ref1 = df.iat[0, 2]
+
+        # Kai Fix Mobile No.
+        mobile = '0830824173' # Kai
+        # mobile = '0814584803' # Nam
+        # mobile = '0844384171' # PFon
+        # mobile = '0860554484' # PKae
+        # mobile = '0922464243' # Nid
+
+        if foreigner == 'F':
+            sms_msg = smsEN
+        else:
+            sms_msg = smsTH
+
+        # Kai Random msg
+        sms_msg = "{} ({})".format(sms_msg, random.randint(500, 50000))
+        # print("SMS Message = {}".format(sms_msg))
+        logging.info("SMS Message = {}".format(sms_msg))
+
+        # Update Status Send Mail Success
+        # updateRefund(hyrf_id=hyrf, send_status='Y')
+        logging.info("Successfully sent sms")
+
+        dataobj = sms_json_model(mobile, sms_msg, ref1)
+        print(dataobj)
+        logging.info(dataobj)
+
+        response = send_sms(dataobj)
+        data = response.json()
+        logging.info(data)
+        # print('Status Code : {}'.format(response.status_code))
+        # print('Status Message : {}'.format(data[0]['SendStatus']))
+        # print('Status Text : {}'.format(data[0]['Result']))
+        logging.info('Status Code : {}'.format(response.status_code))
+        logging.info('Status Message : {}'.format(data[0]['SendStatus']))
+        logging.info('Status Text : {}'.format(data[0]['Result']))
+
+
+def sms_json_model(mobile: str, sms_msg: str, ref1: str):
     dataobj = [{
         "smsid": 0,
         "sendByApp": APP_NAME,
-        "sendDate": "2019-09-04T04:03:06.431Z",
-        "mobileNumber": "0830824173",
-        "sender": "AP-Refund",
-        "ref1": "10133CT9163802",
-        "ref2": "string",
-        "ref3": "string",
-        "messageFrom": "string",
-        "title": "string",
-        "message": "string",
-        "sendStatus": "string",
-        "result": "string",
-        "fileName": "string",
-        "fullPath": "string"
+        "sendDate": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "mobileNumber": mobile,
+        "sender": "APRefund",
+        "ref1": ref1,
+        "ref2": "-",
+        "ref3": "-",
+        "messageFrom": "APRefund",
+        "title": "-",
+        "message": sms_msg,
+        "sendStatus": "",
+        "result": "",
+        "fileName": "",
+        "fullPath": ""
     }]
-
-    response = send_sms(dataobj)
-    data = response.json()
-    print(data)
-    print('Status Code : {}'.format(response.status_code))
-    print('Status Message : {}'.format(data[0]['SendStatus']))
-    print('Status Text : {}'.format(data[0]['Result']))
+    return dataobj
 
 
 if __name__ == '__main__':
     # Get Default Parameter from DB
-    # dfltVal = getDfltParam()
-    #
-    # mailSubject = dfltVal[0]
-    # mailBody = dfltVal[1]
+    dfltVal = getDfltParam()
+
+    smsTH = dfltVal[0]
+    smsEN = dfltVal[1]
     # log_path = dfltVal[2]
-    # mailSubject_en = dfltVal[3]
-    # mailBody_en = dfltVal[4]
-    #
-    # logFile = log_path + '/BatchHappyRefundMailSend.log'
-    #
-    # logging.basicConfig(level=logging.DEBUG,
-    #                     format='%(asctime)-5s [%(levelname)-8s] >> %(message)s',
-    #                     datefmt='%Y-%m-%d %H:%M:%S',
-    #                     filename=logFile,
-    #                     filemode='a')
-    #
-    # logging.debug('#####################')
-    # logging.info('Start Process')
-    main()
-    # logging.info('End Process')
-    # logging.debug('#####################')
+    log_path = '.'
+
+    logFile = log_path + '/BatchHappyRefundSendSMS.log'
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)-5s [%(levelname)-8s] >> %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        filename=logFile,
+                        filemode='a')
+
+    logging.debug('#####################')
+    logging.info('Start Process')
+    main(smsTH, smsEN)
+    logging.info('End Process')
+    logging.debug('#####################')
